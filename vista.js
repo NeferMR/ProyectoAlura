4bffa168e08b5d47bd4322cd6c8261642e76a9fa
NeferMR <ricaurten@uninorte.edu.co>
2023-05-29 18:22:34 -0500

    Entregable terminado

--- vista.js
+++ vista.js
@@ -37,12 +37,99 @@ function copiarTexto() {
   document.execCommand("copy");
 }
 
+function encriptarTexto(texto) {
+  var resultado = '';
+  for (var i = 0; i < texto.length; i++) {
+    switch (texto[i]) {
+      case 'a':
+        resultado += 'ai';
+        break;
+      case 'e':
+        resultado += 'enter';
+        break;
+      case 'i':
+        resultado += 'imes';
+        break;
+      case 'o':
+        resultado += 'ober';
+        break;
+      case 'u':
+        resultado += 'ufat';
+        break;
+      default:
+        resultado += texto[i];
+        break;
+    }
+  }
+  return resultado;
+}
+
+function desencriptarTexto(texto) {
+  var resultado = '';
+  var i = 0;
+  while (i < texto.length) {
+    switch (texto[i]) {
+      case 'a':
+        if (texto[i+1] === 'i'){
+          resultado += 'a';
+          i += 2;
+        } else {
+          resultado += texto[i];
+          i++;
+        }
+        break;
+      case 'e':
+        if (texto[i+1] === 'n' && texto[i+2] === 't' && texto[i+3] === 'e' && texto[i+4] === 'r'){
+          resultado += 'e';
+          i += 5;
+        } else {
+          resultado += texto[i];
+          i++;
+        }  
+        break;
+      case 'i':
+        if (texto[i+1] === 'm' && texto[i+2] === 'e' && texto[i+3] === 's'){
+          resultado += 'i';
+          i += 4;
+        } else {
+          resultado += texto[i];
+          i++;
+        }  
+        break;
+      case 'o':
+        if (texto[i+1] === 'b' && texto[i+2] === 'e' && texto[i+3] === 'r'){
+          resultado += 'o';
+          i += 4;
+        } else {
+          resultado += texto[i];
+          i++;
+        }  
+        break;
+      case 'u':
+        if (texto[i+1] === 'f' && texto[i+2] === 'a' && texto[i+3] === 't'){
+          resultado += 'u';
+          i += 4;
+        } else {
+          resultado += texto[i];
+          i++;
+        }
+        break;
+      default:
+        resultado += texto[i];
+        i++;
+        break;
+    }
+  }
+  return resultado;
+}
+
 // Función para reflejar el texto de la textarea en la tarjeta y el textarea reflejado
 function reflejarTexto() {
   var textarea = document.getElementById("mi-textarea");
   var textoReflejado = document.getElementById("texto-reflejado");
   var textoTarjeta = document.getElementById("texto-tarjeta");
   var texto = document.getElementById("texto-inicial");
+  var btnMostrar = document.getElementById("mostrar-btn");
 
   if (textarea.value === "") {
     texto.style.display = "block"
@@ -50,7 +137,12 @@ function reflejarTexto() {
     mostrarElementosTarjeta();
   } else {
     texto.style.display = "none"
-    textoReflejado.value = textarea.value;
+    console.log(btnMostrar.style.display);
+    if (btnMostrar.style.display === "none") {
+      textoReflejado.value = desencriptarTexto(textarea.value);
+    } else {
+      textoReflejado.value = encriptarTexto(textarea.value);
+    }
     ocultarElementosTarjeta();
     
   }
